993e6ec15fdf3275a09ddf0102e3686714065b57
Tao Wen <taowen@gmail.com>
2025-05-19 09:02:31 +0800

    update

--- vhd-launcher.ps1
+++ vhd-launcher.ps1
@@ -7,5 +7,29 @@ Start-Transcript -Path "vhd-launcher.log" -Append
 
 Write-Host "The vhd file path is: $VhdPath"
 
-Read-Host "Press Enter to exit"
+$IniPath = $VhdPath -replace '\.vhd$', '.ini'
 
+# read .ini file
+$iniContent = Get-Content $IniPath | Where-Object { $_ -match '=' }
+
+# create a hash table to store key-value
+$ini = @{}
+foreach ($line in $iniContent) {
+    $parts = $line -split '=', 2
+    if ($parts.Count -eq 2) {
+        $key = $parts[0].Trim()
+        $value = $parts[1].Trim()
+        $ini[$key] = $value
+    }
+}
+
+# read each configuration
+$readonly = $ini['readonly']
+$launch = $ini['launch']
+$sourceSaveDir = $ini['sourceSaveDir']
+$targetSaveDir = $ini['targetSaveDir']
+
+Write-Host "readonly: $readonly"
+Write-Host "launch: $launch"
+Write-Host "sourceSaveDir: $sourceSaveDir"
+Write-Host "targetSaveDir: $targetSaveDir"
